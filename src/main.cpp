ba532b987180c63185f7e0846e434d03e4f6e00a
Jorge Santos <jorge.madeiras.santos@gmail.com>
2023-01-08 16:51:25 +0000

    SDL_Image

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
+/*This source code copyrighted by Lazy Foo' Productions (2004-2022)
+and may not be redistributed without written permission.*/
+
+//Using SDL, SDL_image, standard IO, math, and strings
 #include <SDL.h>
 #include <iostream>
+
 
 int main(int argc, char* argv[]){
 
