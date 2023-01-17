0ffd6d0251d4d3a8166c66500fde5a0b03548b74
Jorge Santos <jorge.madeiras.santos@gmail.com>
2023-01-17 19:50:42 +0000

    Commit 17.01 - Testing Renderer

--- src/entity.cpp
+++ src/entity.cpp
@@ -1,13 +1,16 @@
 #include "entity.h"
-#include <iostream>
-#include <SDL.h>
-#include <SDL_image.h>
 
-Entity::Entity(const Window &window, const char *texPath, int x, int y, int w, int h) : 
-Window(window), _texPath(texPath), _x(x), _y(y), _w(w), _h(h)
+
+Entity::Entity(const char *texPath, int x, int y, int w, int h) : 
+ _texPath(texPath), _x(x), _y(y), _w(w), _h(h)
 {
- 
-    SDL_Surface* surf = IMG_Load(_texPath);
+    
+
+};
+
+void Entity::setTex(SDL_Renderer* _renderer){
+
+ SDL_Surface* surf = IMG_Load(_texPath);
    
     if(!surf) {
 
@@ -17,35 +20,51 @@ Window(window), _texPath(texPath), _x(x), _y(y), _w(w), _h(h)
 
     _tex = SDL_CreateTextureFromSurface(_renderer, surf);
 
-};
-
-
-
-
-void Entity::render() {
-    SDL_Rect src;
-    src.x = 0;
-    src.y = 0;
-    src.w = _w;
-    src.h = _h;
-
-    SDL_Rect dst;
-    dst.x = _x;
-    dst.y = _y;
-    dst.w = _w;
-    dst.h = _h;
-    
-       
-        SDL_RenderCopy(_renderer,_tex,&src,&dst);
-        SDL_RenderPresent(_renderer);
-        SDL_RenderClear(_renderer);
-
-    
-   
-    
-    
 }
 
+SDL_Texture* Entity::getTex(){
+
+    return _tex;
+}
+
+void Entity::setSrc(){
+    
+    _src.x = 0;
+    _src.y = 0;
+    _src.w = _w;
+    _src.h = _h;
+   
+}
+
+SDL_Rect Entity::getSrc()
+ {
+
+    return _src;
+}
+
+void Entity::setDst(){
+    
+    _dst.x = _x;
+    _dst.y = _y;
+    _dst.w = _w;
+    _dst.h = _h;
+}
+
+SDL_Rect Entity::getDst(){
+ 
+    return _dst;
+
+}
+
+
+
+void Entity::draw(SDL_Renderer* _renderer){
+
+
+
+
+
+}
 
 void Entity::moveEvent(SDL_Event event){
 
