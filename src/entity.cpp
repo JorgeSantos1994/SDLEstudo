677d66a8f0bc2b03b42617495dba51ca86b3f218
Jorge Santos <jorge.madeiras.santos@gmail.com>
2023-01-18 02:25:52 +0000

    Commit 1 17.01

--- src/entity.cpp
+++ src/entity.cpp
@@ -1,14 +1,15 @@
 #include "entity.h"
 
 
-Entity::Entity(const char *texPath, int x, int y, int w, int h) : 
- _texPath(texPath), _x(x), _y(y), _w(w), _h(h)
+Entity::Entity(const char *texPath, SDL_Renderer* renderer, int x, int y, int w, int h) : 
+ _texPath(texPath), _x(x), _y(y), _w(w), _h(h), _renderer(renderer)
 {
     
+setTex();
 
 };
 
-void Entity::setTex(SDL_Renderer* _renderer){
+void Entity::setTex(){
 
  SDL_Surface* surf = IMG_Load(_texPath);
    
@@ -27,7 +28,7 @@ SDL_Texture* Entity::getTex(){
     return _tex;
 }
 
-void Entity::setSrc(){
+void Entity::setSrc(int _x, int _y){
     
     _src.x = 0;
     _src.y = 0;
@@ -42,7 +43,7 @@ SDL_Rect Entity::getSrc()
     return _src;
 }
 
-void Entity::setDst(){
+void Entity::setDst(int _x, int _y){
     
     _dst.x = _x;
     _dst.y = _y;
@@ -56,15 +57,37 @@ SDL_Rect Entity::getDst(){
 
 }
 
+void Entity :: drawEntity() {
 
+    setSrc(_x, _y);
+    setDst(_x, _y);
+    SDL_RenderCopy(_renderer,_tex,&_src,&_dst);
 
-void Entity::draw(SDL_Renderer* _renderer){
-
-
-
-
+    
 
 }
+
+
+
+void Entity :: fallEvent(SDL_Event event, Mousecontroller& mouse, int screenWith, int screenHeight, Uint32 tick, Uint32 elapsed) {
+ 
+
+    std::cout << _dst.y << " : " << screenHeight << std::endl;
+
+    if(mouse.mx <= _dst.x + _dst.w && mouse.mx >= _dst.x  && mouse.my <= _dst.y + _dst.h && mouse.my >= _dst.y)  {
+          if(_dst.y < screenHeight - 100){
+            if(mouse.mLeft == true){
+        
+                _y += 10;
+                           
+            }
+            }               
+        }                  
+      
+}
+
+
+
 
 void Entity::moveEvent(SDL_Event event){
 
